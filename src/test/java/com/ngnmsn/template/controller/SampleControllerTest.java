2e2348a716ab2316ed02c9395ee35e0ef476380c
ngnmsn <ngnmsn.dev@gmail.com>
2024-06-27 00:36:57 +0900

    ControllerクラスのUT実装（登録）

--- src/test/java/com/ngnmsn/template/controller/SampleControllerTest.java
+++ src/test/java/com/ngnmsn/template/controller/SampleControllerTest.java
@@ -1,22 +1,21 @@
 package com.ngnmsn.template.controller;
 
+import com.ngnmsn.template.domain.sample.SampleCreateForm;
 import com.ngnmsn.template.domain.sample.SampleResult;
-import com.ngnmsn.template.domain.sample.SampleSearchForm;
-import com.ngnmsn.template.repository.impl.SampleRepositoryImpl;
 import com.ngnmsn.template.service.SampleService;
+import jakarta.servlet.http.HttpSession;
 import org.jooq.types.ULong;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
@@ -36,6 +35,9 @@ public class SampleControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Mock
+    private HttpSession session;
 
     @InjectMocks
     private SampleController sampleController;
@@ -81,5 +83,92 @@ public class SampleControllerTest {
                 .post("/sample/search").params(formMap))
                 .andExpect(status().isOk())
                 .andExpect(view().name("sample/list"));
+    }
+
+    @DisplayName("detail()の正常系テスト")
+    @Test
+    public void testDetailSuccess() throws Exception{
+
+        SampleResult expect = new SampleResult() {{
+            setId(ULong.valueOf(1));
+            setDisplayId("001ABCDEFGHIJKLMNOPQRSTUVWXYZABC");
+            setText1("test1");
+            setNum1(1);
+        }};
+
+        when(sampleService.detail(any())).thenReturn(expect);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/sample/001ABCDEFGHIJKLMNOPQRSTUVWXYZABC"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sample/detail"));
+    }
+
+    @DisplayName("create()の正常系テスト")
+    @Test
+    public void testCreateSuccess() throws Exception{
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/sample/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sample/create"));
+    }
+
+    @DisplayName("createConfirm()の正常系テスト")
+    @Test
+    public void testCreateConfirmSuccess() throws Exception{
+
+        final MultiValueMap<String, String> formMap = new LinkedMultiValueMap<>() {{
+            add("text1", "test1");
+            add("num1", "1");
+        }};
+
+        doNothing().when(session).setAttribute(any(),any());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/sample/create/confirm")
+                        .params(formMap))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sample/create_confirm"));
+    }
+
+    @DisplayName("returnCreate()の正常系テスト")
+    @Test
+    public void testReturnCreateSuccess() throws Exception{
+
+        SampleCreateForm form = new SampleCreateForm() {{
+            setText1("test1");
+            setNum1(1);
+        }};
+
+        when(session.getAttribute(any())).thenReturn(form);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/sample/create/confirm"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sample/create"));
+    }
+
+    @DisplayName("createProcess()の正常系テスト")
+    @Test
+    public void testCreateProcessSuccess() throws Exception{
+
+        SampleCreateForm form = new SampleCreateForm() {{
+            setText1("test1");
+            setNum1(1);
+        }};
+
+        when(session.getAttribute(any())).thenReturn(form);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/sample/create/process"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/sample/create/complete"));
+    }
+
+    @DisplayName("createComplete()の正常系テスト")
+    @Test
+    public void testCreateCompleteSuccess() throws Exception{
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/sample/create/complete"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sample/create_complete"));
     }
 }
