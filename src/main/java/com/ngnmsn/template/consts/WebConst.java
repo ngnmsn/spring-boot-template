ce2e26a05864b92ba6d71b4ef0ebfce5c406005a
ngnmsn <ngnmsn.dev@gmail.com>
2024-07-06 14:54:16 +0900

    Constクラスの実装

---
+++ src/main/java/com/ngnmsn/template/consts/WebConst.java
@@ -0,0 +1,28 @@
+package com.ngnmsn.template.consts;
+
+public class WebConst {
+  public static final String URL_SEARCH = "/search";
+  public static final String URL_DETAIL = "/{displayId}";
+  public static final String URL_CREATE = "/create";
+  public static final String URL_CREATE_CONFIRM = "/create/confirm";
+  public static final String URL_CREATE_PROCESS = "/create/process";
+  public static final String URL_CREATE_COMPLETE = "/create/complete";
+  public static final String URL_UPDATE = "/{displayId}/update";
+  public static final String URL_UPDATE_CONFIRM = "/{displayId}/update/confirm";
+  public static final String URL_UPDATE_PROCESS = "/{displayId}/update/process";
+  public static final String URL_UPDATE_COMPLETE = "/{displayId}/update/complete";
+  public static final String URL_DELETE_CONFIRM = "/{displayId}/delete/confirm";
+  public static final String URL_DELETE_PROCESS = "/{displayId}/delete/process";
+  public static final String URL_DELETE_COMPLETE = "/{displayId}/delete/complete";
+
+  public static final String TEMPLATE_LIST = "list";
+  public static final String TEMPLATE_DETAIL = "detail";
+  public static final String TEMPLATE_CREATE = "create";
+  public static final String TEMPLATE_CREATE_CONFIRM = "create_confirm";
+  public static final String TEMPLATE_CREATE_COMPLETE = "create_complete";
+  public static final String TEMPLATE_UPDATE = "update";
+  public static final String TEMPLATE_UPDATE_CONFIRM = "update_confirm";
+  public static final String TEMPLATE_UPDATE_COMPLETE = "update_complete";
+  public static final String TEMPLATE_DELETE_CONFIRM = "delete_confirm";
+  public static final String TEMPLATE_DELETE_COMPLETE = "delete_complete";
+}
--- src/main/java/com/ngnmsn/template/controller/SampleController.java
+++ src/main/java/com/ngnmsn/template/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.ngnmsn.template.controller;
 
+import com.ngnmsn.template.consts.SampleConst;
+import com.ngnmsn.template.consts.WebConst;
 import com.ngnmsn.template.domain.sample.SampleCreateForm;
 import com.ngnmsn.template.domain.sample.SampleDeleteForm;
 import com.ngnmsn.template.domain.sample.SampleResult;
@@ -19,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/sample")
+@RequestMapping(value = SampleConst.URL_SAMPLE)
 public class SampleController {
 
   @Autowired
@@ -32,58 +34,58 @@ public class SampleController {
   String list(Model model) {
     SampleSearchForm sampleSearchForm = new SampleSearchForm();
     model.addAttribute("sampleSearchForm", sampleSearchForm);
-    return "sample/list";
+    return SampleConst.TEMPLATE_SAMPLE_LIST;
   }
 
-  @PostMapping("/search")
+  @PostMapping(WebConst.URL_SEARCH)
   String search(@ModelAttribute("sampleSearchForm") SampleSearchForm form, Model model) {
     List<SampleResult> sampleResults = sampleService.search(form);
     model.addAttribute("sampleResults", sampleResults);
-    return "sample/list";
+    return SampleConst.TEMPLATE_SAMPLE_LIST;
   }
 
-  @GetMapping("/{displayId}")
+  @GetMapping(WebConst.URL_DETAIL)
   String detail(@PathVariable String displayId, Model model) {
     SampleResult sampleResult = sampleService.detail(displayId);
     model.addAttribute("sampleResult", sampleResult);
-    return "sample/detail";
+    return SampleConst.TEMPLATE_SAMPLE_DETAIL;
   }
 
-  @GetMapping("/create")
+  @GetMapping(WebConst.URL_CREATE)
   String create(Model model) {
     SampleCreateForm sampleCreateForm = new SampleCreateForm();
     model.addAttribute("sampleCreateForm", sampleCreateForm);
-    return "sample/create";
+    return SampleConst.TEMPLATE_SAMPLE_CREATE;
   }
 
-  @PostMapping("/create/confirm")
+  @PostMapping(WebConst.URL_CREATE_CONFIRM)
   String createConfirm(@ModelAttribute("sampleCreateForm") SampleCreateForm form, Model model) {
     session.setAttribute("sampleCreateForm", form);
     model.addAttribute("sampleCreateForm", form);
-    return "sample/create_confirm";
+    return SampleConst.TEMPLATE_SAMPLE_CREATE_CONFIRM;
   }
 
-  @GetMapping("/create/confirm")
+  @GetMapping(WebConst.URL_CREATE_CONFIRM)
   String returnCreate(Model model) {
     SampleCreateForm form = (SampleCreateForm) session.getAttribute("sampleCreateForm");
     model.addAttribute("sampleCreateForm", form);
-    return "sample/create";
+    return SampleConst.TEMPLATE_SAMPLE_CREATE;
   }
 
-  @PostMapping("/create/process")
+  @PostMapping(WebConst.URL_CREATE_PROCESS)
   String createProcess() {
     SampleCreateForm createForm = (SampleCreateForm) session.getAttribute("sampleCreateForm");
     sampleService.create(createForm);
     session.removeAttribute("sampleCreateForm");
-    return "redirect:/sample/create/complete";
+    return SampleConst.REDIRECT_SAMPLE_CREATE_COMPLETE;
   }
 
-  @GetMapping("/create/complete")
+  @GetMapping(WebConst.URL_CREATE_COMPLETE)
   String createComplete() {
-    return "sample/create_complete";
+    return SampleConst.TEMPLATE_SAMPLE_CREATE_COMPLETE;
   }
 
-  @GetMapping("/{displayId}/update")
+  @GetMapping(WebConst.URL_UPDATE)
   String update(@PathVariable String displayId, Model model) {
     SampleResult sampleResult = sampleService.detail(displayId);
     SampleUpdateForm form = new SampleUpdateForm();
@@ -93,26 +95,26 @@ public class SampleController {
     session.setAttribute("sampleUpdateDisplayId", sampleResult.getDisplayId());
     model.addAttribute("sampleUpdateDisplayId", sampleResult.getDisplayId());
     model.addAttribute("sampleUpdateForm", form);
-    return "sample/update";
+    return SampleConst.TEMPLATE_SAMPLE_UPDATE;
   }
 
-  @PostMapping("/{displayId}/update/confirm")
+  @PostMapping(WebConst.URL_UPDATE_CONFIRM)
   String updateConfirm(@ModelAttribute("sampleUpdateForm") SampleUpdateForm form, Model model) {
     String displayId = (String) session.getAttribute("sampleUpdateDisplayId");
     session.setAttribute("sampleUpdateForm", form);
     model.addAttribute("sampleUpdateDisplayId", displayId);
     model.addAttribute("sampleUpdateForm", form);
-    return "sample/update_confirm";
+    return SampleConst.TEMPLATE_SAMPLE_UPDATE_CONFIRM;
   }
 
-  @GetMapping("/{displayId}/update/confirm")
+  @GetMapping(WebConst.URL_UPDATE_CONFIRM)
   String returnUpdate(Model model) {
     SampleUpdateForm form = (SampleUpdateForm) session.getAttribute("sampleUpdateForm");
     model.addAttribute("sampleUpdateForm", form);
-    return "sample/update";
+    return SampleConst.TEMPLATE_SAMPLE_UPDATE;
   }
 
-  @PostMapping("/{displayId}/update/process")
+  @PostMapping(WebConst.URL_UPDATE_PROCESS)
   String updateProcess() {
     ULong sampleUpdateId = (ULong) session.getAttribute("sampleUpdateId");
     SampleUpdateForm updateForm = (SampleUpdateForm) session.getAttribute("sampleUpdateForm");
@@ -120,15 +122,15 @@ public class SampleController {
     session.removeAttribute("sampleUpdateId");
     session.removeAttribute("sampleUpdateDisplayId");
     session.removeAttribute("sampleUpdateForm");
-    return "redirect:/sample/{displayId}/update/complete";
+    return SampleConst.REDIRECT_SAMPLE_UPDATE_COMPLETE;
   }
 
-  @GetMapping("/{displayId}/update/complete")
+  @GetMapping(WebConst.URL_UPDATE_COMPLETE)
   String updateComplete() {
-    return "sample/update_complete";
+    return SampleConst.TEMPLATE_SAMPLE_UPDATE_COMPLETE;
   }
 
-  @GetMapping("/{displayId}/delete/confirm")
+  @GetMapping(WebConst.URL_DELETE_CONFIRM)
   String deleteConfirm(@PathVariable String displayId, Model model) {
     SampleResult sampleResult = sampleService.detail(displayId);
     SampleDeleteForm form = new SampleDeleteForm();
@@ -138,20 +140,20 @@ public class SampleController {
     session.setAttribute("sampleDeleteDisplayId", sampleResult.getDisplayId());
     model.addAttribute("sampleDeleteDisplayId", sampleResult.getDisplayId());
     model.addAttribute("sampleDeleteForm", form);
-    return "sample/delete_confirm";
+    return SampleConst.TEMPLATE_SAMPLE_DELETE_CONFIRM;
   }
 
-  @PostMapping("/{displayId}/delete/process")
+  @PostMapping(WebConst.URL_DELETE_PROCESS)
   String deleteProcess() {
     ULong sampleDeleteId = (ULong) session.getAttribute("sampleDeleteId");
     sampleService.delete(sampleDeleteId);
     session.removeAttribute("sampleDeleteId");
     session.removeAttribute("sampleDeleteDisplayId");
-    return "redirect:/sample/{displayId}/delete/complete";
+    return SampleConst.REDIRECT_SAMPLE_DELETE_COMPLETE;
   }
 
-  @GetMapping("/{displayId}/delete/complete")
+  @GetMapping(WebConst.URL_DELETE_COMPLETE)
   String deleteComplete() {
-    return "sample/delete_complete";
+    return SampleConst.TEMPLATE_SAMPLE_DELETE_COMPLETE;
   }
 }
