3b8d58d71d0421d5383d152520812fa76e60ed5e
ngnmsn <ngnmsn.dev@gmail.com>
2024-07-13 18:16:48 +0900

    ツールチップの有効化と実装

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,14 @@
 $(function() {
+
+  // tooltipの初期化
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  // IDフル桁コピー
   $('#copy').on('click',function(){
     const id = $(this).data('id');
     navigator.clipboard.writeText(id);
-    console.log("copyしました。");
   });
 });
